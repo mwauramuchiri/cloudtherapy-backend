2c2999125d9fc356b942e6a406e1e1fbee1e971f
AllanJeremy <aj.dev254@gmail.com>
2020-02-18 01:12:15 +0300

    [Cleanup] Cleaned up the API util

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,23 +1,23 @@
-module.exports.getResponse = (isOk, message, data, statusCode) => {
+module.exports.getResponse = (isOk, message, data, statusCode = 200) => {
     let response = {};
     response.ok = isOk;
-    response.statusCode = statusCode || 200;
+    response.statusCode = statusCode;
     response.message = message;
     response.data = data || null;
     return response;
 };
 
-module.exports.getError = (message, error, statusCode) => {
-    statusCode = statusCode || 500;
+module.exports.getError = (message, error, statusCode = 500) => {
+    statusCode = statusCode;
     let response = this.getResponse(false, message, null, statusCode);
     response.error = error;
     return response;
 };
 
 // Attaches an error handler to a function ~ returns an appropriate response
-module.exports.attachErrorHandler = (res,fn)=>{ //? Intended to be called in middleware
-    return fn.catch(err=>{
-        let apiResponse = this.getError(err.message,err);
+module.exports.attachErrorHandler = (res, fn) => { //? Intended to be called in middleware
+    return fn.catch(err => {
+        let apiResponse = this.getError(err.message, err);
         console.log(err);
         console.error(err.message);
 
