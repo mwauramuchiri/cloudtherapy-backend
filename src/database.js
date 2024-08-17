172414c136a640d2ee0bb2c8d96e13becc578c93
mwauramuchiri <gramwauu@gmail.com>
2024-08-17 18:48:36 +0300

    moved secrets to root

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./../credentials/ementoring-afric-1536067843571-firebase-adminsdk-cxwi3-9c68eb4b9c.json");
+var serviceAccount = require("./../ementoring-afric-1536067843571-firebase-adminsdk-cxwi3-9c68eb4b9c.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
